f504b4ec5b2f969b4843810c4e244d62c6b176d0
Paramveer <singhp35@mymail.vcu.edu>
2022-01-11 18:16:00 -0500

    Made Connection to DBMS

--- src/main/java/com/store/accounting/AccountingService.java
+++ src/main/java/com/store/accounting/AccountingService.java
@@ -1,16 +1,39 @@
 package com.store.accounting;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
 public class AccountingService {
 
     dailyReport report;
+    monthlyReport monthlyReport;
+    private AccountingRepo repo;
 
-    public AccountingService(){}
+    @Autowired
+    public AccountingService(AccountingRepo r){
+        this.repo = r;
+    }
 
+    @Transactional
     public void reportDaily(dailyReport r){
         report = new dailyReport(r);
-        System.out.println(r.getSales());
-        System.out.println(r.getTaxCollected());
-        System.out.println(r.getRegularFuelSales());
+        pushDb(report);
+        System.out.println(report.toString());
         //Add to Database
     }
+
+    public dailyReport getMonthlyReport() {
+        return null;
+    }
+
+
+    private void pushDb(dailyReport r){
+        repo.updateDaily(r.getDate(), r.getDrinksSales(), r.getHotFoodSales(), r.getRegularFuelSales(), r.getSales(), r.getSnackSales(), r.getTaxCollected(), r.getTaxRate());
+    }
+
+
+
 }
