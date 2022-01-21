fff8069d686d4674e1f19cc930c431bc08a555c6
Paramveer <singhp35@mymail.vcu.edu>
2022-01-21 15:40:03 -0500

    Cleaned up Unncessary Code

--- src/main/java/com/store/inventory/InventoryController.java
+++ src/main/java/com/store/inventory/InventoryController.java
@@ -11,49 +11,24 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "inventory/all")
-class InventoryController {
+class inventory {
 
     Inventory i = new Inventory();
 
-    @GetMapping
-    public List get(){
-        //return i.getInventory();
-        return List.of(
-                new Drink("Pepsi", 1.99, 5, 1234),
-                new Drink("Diet Pepsi", 1.99, 5, 1235),
-                new Food("Lays", 1.99, 5, 9876),
-                new Food("Kit-Kat", 1.99, 5, 9875),
-                new Drink("Coke", 1.99, 5, 2345),
-                new Drink("Diet Coke", 1.99, 5, 2346)
-        );
-    }
-}
-
-@RestController
-@RequestMapping(path = "inventory")
-class home {
-
-    @GetMapping
-    public String get(){
+    @GetMapping(path = "inventory")
+    public String home(){
         return "Hello, use inventory/all endpoint to view all stock";
     }
-}
 
-@RestController
-@RequestMapping(path = "inventory/delivery")
-class delivery {
-
-    @PutMapping
-    public List get(){
-        //return i.getInventory();
-        return List.of(
-                new Drink("Pepsi", 1.99, 5, 1234),
-                new Drink("Diet Pepsi", 1.99, 5, 1235),
-                new Food("Lays", 1.99, 5, 9876),
-                new Food("Kit-Kat", 1.99, 5, 9875),
-                new Drink("Coke", 1.99, 5, 2345),
-                new Drink("Diet Coke", 1.99, 5, 2346)
-        );
+    @GetMapping(path = "inventory/current")
+    public List getAll(){
+        return i.getInventory();
     }
+
+    @PutMapping(path = "inventory/delivery")
+    public void add(){
+
+    }
+
 }
+
