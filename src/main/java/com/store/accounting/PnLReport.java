62e26278e9bd0fec5e8450084fbffe9b9669597e
Paramveer <singhp35@mymail.vcu.edu>
2022-02-11 01:10:14 -0500

    Pnl Report Generator

--- src/main/java/com/store/accounting/PnLReport.java
+++ src/main/java/com/store/accounting/PnLReport.java
@@ -1,25 +1,75 @@
 package com.store.accounting;
 
+
+import com.google.gson.Gson;
+
+
+
 public class PnLReport {
+    Gson gson = new Gson();
+
     double sales = 0;
     //Cost of Goods Sold
     double cogs = 0;
     double grossProfit = 0;
+    double netProfit = 0;
 
-
+    //Expenses will be replaced by actual expenses such as Payroll and General Overhead
     double expenses = 0;
-
 
     //Cogs will be pulled from Daily Report
     //Cash Register does report Cost of Goods Sold
     //Accounting Service does not take in Cogs yet
-    PnLReport(double s, double cogs){
-        this.sales = s;
-        this.cogs = 0;
+    //The arguments will come from a Monthly Report
+    //Not yet implemented
+    //Hard Coding monthlySale and monnthlyCogs
+    PnLReport(double monthlySale, double monthlyCogs) {
+    //    this.sales = 5000.00;
+      //  this.cogs = 100.00;
     }
 
-    double calcGross(){ return grossProfit = sales - cogs; }
+    PnLReport(){}
+
+    void setMonthlySales(){this.sales = 5000.00; this.cogs = 100.00;}
 
 
+    void calcGross() {
+        this.grossProfit = sales - cogs;
+    }
 
+    void calcOverHead() {
+        this.expenses = 200;
+    }
+
+    void calcNet() {
+        this.netProfit = grossProfit - expenses;
+    }
+
+    String generatePnL(){
+        setMonthlySales();
+        calcGross();
+        calcOverHead();
+        calcNet();
+        double[] n = {sales, cogs, grossProfit, expenses, netProfit};
+        return gson.toJson(n);
+    }
 }
+
+
+
+
+
+
+//    public static void main(String[] args){
+//
+//
+//        int[] numbers = {1, 1, 2, 3, 5, 8, 13};
+//        String numbersJson = gson.toJson(numbers);
+//
+//
+//        System.out.println(numbersJson);
+//    }
+
+
+
+
