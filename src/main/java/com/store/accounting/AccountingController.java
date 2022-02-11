6d387988453bcf756fb239b2759119f5f75b9467
Paramveer <singhp35@mymail.vcu.edu>
2022-02-11 01:08:01 -0500

    Returns Profit and Loss in Json

--- src/main/java/com/store/accounting/AccountingController.java
+++ src/main/java/com/store/accounting/AccountingController.java
@@ -4,31 +4,53 @@ package com.store.accounting;
 
 import com.store.products.Drink;
 import com.store.products.Food;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 
+
+//import java.sql.Date;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 @RestController
-class AccountingController {
-    @GetMapping("accounting")
+class AccountingController <k,v>{
+
+    AccountingService service;
+    final String base = "accounting";
+
+    @GetMapping(base)
     public String main(){
         return "Accounting Main Page";
     }
 
-    @GetMapping("hello")
+    @GetMapping(base + "/hello")
     public String test(){
         return "THIS WORKSSSSSS";
     }
 
-    AccountingService s = new AccountingService();
-
-    @PostMapping(path = "accounting/daily")
-    public void d(@RequestBody dailyReport r){
-        s.reportDaily(r);
+    @Autowired
+    public AccountingController(AccountingService a){
+        this.service = a;
     }
 
-    @GetMapping(path = "accounting/monthly")
-    public void m(@RequestBody dailyReport r){
-        s.reportDaily(r);
+    @PostMapping(path = base + "/daily")
+    //public void d(@RequestBody TreeMap<k, v> map){
+    public void d(@RequestBody dailyReport r){
+     //   dailyReport s = (dailyReport) map.get("dailyReport");
+      //  service.reportDaily((dailyReport) map.get("dailyReport"));
+     service.reportDaily(r);
+    }
+
+    @GetMapping(path = base +"/monthly")
+    public void m(){
+        //Generate Monthly Report
+    }
+
+    @GetMapping(path = base+"/pnl")
+    public String pnl(){
+        return service.generatePnL();
     }
 }
