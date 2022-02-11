e71083b0f82d7bc68b18a078be65e2cc647a2c06
Paramveer <singhp35@mymail.vcu.edu>
2022-02-11 01:09:48 -0500

    Calls Pnl Generator

--- src/main/java/com/store/accounting/AccountingService.java
+++ src/main/java/com/store/accounting/AccountingService.java
@@ -5,12 +5,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.google.gson.Gson;
+
+
 @Service
 public class AccountingService {
 
     dailyReport report;
     monthlyReport monthlyReport;
     private AccountingRepo repo;
+    PnLReport pnlreport = new PnLReport();
 
     @Autowired
     public AccountingService(AccountingRepo r){
@@ -29,11 +33,12 @@ public class AccountingService {
         return null;
     }
 
+    public String generatePnL(){
+        return pnlreport.generatePnL();
+    }
+
 
     private void pushDb(dailyReport r){
         repo.updateDaily(r.getDate(), r.getDrinksSales(), r.getHotFoodSales(), r.getRegularFuelSales(), r.getSales(), r.getSnackSales(), r.getTaxCollected(), r.getTaxRate());
     }
-
-
-
 }
