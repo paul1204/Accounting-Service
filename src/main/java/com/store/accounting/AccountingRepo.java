4b0a2b042a9dbdc9df4761ff41628658f9c5774c
Paramveer <singhp35@mymail.vcu.edu>
2022-09-28 18:30:44 -0400

    Removed unused query

--- src/main/java/com/store/accounting/AccountingRepo.java
+++ src/main/java/com/store/accounting/AccountingRepo.java
@@ -11,18 +11,6 @@ import java.util.List;
 @Repository
 public interface AccountingRepo extends JpaRepository<dailyReport, Double> {
 
-//    @Modifying
-//    @Query(value = "INSERT into daily_report " +
-//            "(date, drinks_sales, hot_food_sales, regular_fuel_sales, sales, snack_sales, tax_collected, tax_rate) values (:date, :drink_sales, :hot_food_sales, :regular_fuel_sales, :sales, :snack_sales, :tax_collected, :tax_rate)", nativeQuery = true)
-//    void updateDaily(@Param("date") String date, @Param("drink_sales") Double drink_sales,
-//                     @Param("hot_food_sales") Double hot_food_sales,
-//                     @Param("regular_fuel_sales") Double regular_fuel_sales,
-//                     @Param("sales") Double sales,
-//                     @Param("snack_sales") Double snack_sales,
-//                     @Param("tax_collected") Double tax_collected,
-//                     @Param("tax_rate") Double tax_rate
-//                     );
-
     @Modifying
     @Query(value = "INSERT into daily_sales " +
             "(total_Sales, tax_collected, quantitySold, cogs) values (:total_Sales, :tax_collected, :quantitySold, :cogs)", nativeQuery = true)
@@ -36,9 +24,4 @@ public interface AccountingRepo extends JpaRepository<dailyReport, Double> {
 
     @Query(value = "SELECT * from daily_sales", nativeQuery = true)
     List<String> m();
-
-
-
 }
-
-//CREATE TABLE daily_sales( total_sales float(2), tax_collected float(2), quantitySold int, cogs float(2) );
